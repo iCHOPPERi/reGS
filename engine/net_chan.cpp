3bd1fa66a684cfd138730529916acd1ef542b811
ScriptedSnark <sevcenkobogdan855@gmail.com>
2022-03-11 22:15:11 +0500

    Reverse Netchan_Init

--- engine/net_chan.cpp
+++ engine/net_chan.cpp
@@ -4,9 +4,23 @@
 //TODO: should be MAX_PATH - Solokiller
 char gDownloadFile[ 256 ] = {};
 
+cvar_t net_log = { "net_log", "0", 0, 0.0f, nullptr };
+cvar_t net_showpackets = { "net_showpackets", "0", 0, 0.0f, nullptr };
+cvar_t net_showdrop = { "net_showdrop", "0", 0, 0.0f, nullptr };
+cvar_t net_drawslider = { "net_drawslider", "0", 0, 0.0f, nullptr };
+cvar_t net_chokeloopback = { "net_chokeloop", "0", 0, 0.0f, nullptr };
+cvar_t sv_filetransfercompression = { "sv_filetransfercompression", "1", 0, 0.0f, nullptr };
+cvar_t sv_filetransfermaxsize = { "sv_filetransfermaxsize", "10485760", 0, 0.0f, nullptr };
+
 void Netchan_Init()
 {
-	//TODO: implement - Solokiller
+	Cvar_RegisterVariable(&net_log);
+	Cvar_RegisterVariable(&net_showpackets);
+	Cvar_RegisterVariable(&net_showdrop);
+	Cvar_RegisterVariable(&net_chokeloopback);
+	Cvar_RegisterVariable(&net_drawslider);
+	Cvar_RegisterVariable(&sv_filetransfercompression);
+	Cvar_RegisterVariable(&sv_filetransfermaxsize);
 }
 
 void Netchan_Transmit( netchan_t* chan, int length, byte* data )
