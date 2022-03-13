e7cc2e6d8a00c2e65b55da2d14b9ff3cd20171e4
ScriptedSnark <sevcenkobogdan855@gmail.com>
2022-03-13 20:44:03 +0500

    Implement Mod_Init and CM_FreePAS

--- engine/cmodel.cpp
+++ engine/cmodel.cpp
@@ -1,12 +1,21 @@
 #include "quakedef.h"
 #include "cmodel.h"
 
+unsigned char* gPAS;
+unsigned char* gPVS;
+unsigned char mod_novis[MODEL_MAX_PVS];
+
 void Mod_Init()
 {
-	//TODO: implement - Solokiller
+	Q_memset(mod_novis, 255, 1024);
 }
 
 void CM_FreePAS()
 {
-	//TODO: implement - Solokiller
+	if (gPAS)
+		Mem_Free(gPAS);
+	if (gPVS)
+		Mem_Free(gPVS);
+	gPAS = 0;
+	gPVS = 0;
 }
--- engine/cmodel.h
+++ engine/cmodel.h
@@ -1,6 +1,8 @@
 #ifndef ENGINE_MODEL_H
 #define ENGINE_MODEL_H
 
+const int MODEL_MAX_PVS = 1024;
+
 void Mod_Init();
 
 void CM_FreePAS();
