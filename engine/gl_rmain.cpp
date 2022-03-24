f8b206e6dfe4080b1013fd6db4d821a8ae7b776d
ScriptedSnark <sevcenkobogdan855@gmail.com>
2022-03-24 12:26:06 +0500

    Implement CalcFov

--- engine/gl_rmain.cpp
+++ engine/gl_rmain.cpp
@@ -32,3 +32,25 @@ void AllowFog( bool allowed )
             qglDisable(GL_FOG);
     }
 }
+
+float CalcFov(float fov_x, float width, float height) // TODO: improve the code - ScriptedSnark
+{
+    long double fov; // fst7
+    float x; // [esp+0h] [ebp-2Ch]
+
+    if (fov_x < 1.0)
+    {
+        fov = 0.9999999999999999;
+    }
+    else if (fov_x <= 179.0)
+    {
+        fov = tan(fov_x / 360.0 * M_PI);
+    }
+    else
+    {
+        fov = 0.9999999999999999;
+    }
+    x = height / (width / fov);
+
+    return atanf(x) * 360.0 / M_PI;
+}
