9dab7189df4bdea312358c0f50b84252cea22d94
ScriptedSnark <sevcenkobogdan855@gmail.com>
2022-03-24 19:40:20 +0500

    Implement HPAK_FindResource, HPAK_ResourceForHash, add HPAK structs

--- engine/hashpak.cpp
+++ engine/hashpak.cpp
@@ -15,6 +15,11 @@ void HPAK_FlushHostQueue()
 	}
 }
 
+void HPAK_AddLump(bool bUseQueue, const char* pakname, resource_t* pResource, void* pData, FileHandle_t fpSource)
+{
+	//TODO: implement - Solokiller
+}
+
 void HPAK_Init()
 {
 	/*
@@ -31,13 +36,87 @@ void HPAK_CheckIntegrity( const char* pakname )
 	//TODO: implement - Solokiller
 }
 
-void HPAK_AddLump( bool bUseQueue, const char* pakname, resource_t* pResource, void* pData, FileHandle_t fpSource )
+qboolean HPAK_FindResource(hash_pack_directory_t* pDir, unsigned char* hash, struct resource_s* pResourceEntry)
 {
-	//TODO: implement - Solokiller
+	for (int i = 0; i < pDir->nEntries; i++)
+	{
+		if (Q_memcmp(hash, pDir->p_rgEntries[i].resource.rgucMD5_hash, 16) == 0)
+		{
+			if (pResourceEntry)
+				Q_memcpy(pResourceEntry, &pDir->p_rgEntries[i].resource, sizeof(resource_t));
+
+			return TRUE;
+		}
+	}
+	return FALSE;
 }
 
-int HPAK_ResourceForHash( const char* pakname, byte* hash, resource_t* pResourceEntry )
+qboolean HPAK_ResourceForHash(char* pakname, unsigned char* hash, struct resource_s* pResourceEntry)
 {
-	//TODO: implement - Solokiller
-	return 0;
+	qboolean bFound;
+	hash_pack_header_t header;
+	hash_pack_directory_t directory;
+	char name[MAX_PATH];
+	FileHandle_t fp;
+
+	if (gp_hpak_queue)
+	{
+		for (hash_pack_queue_t* p = gp_hpak_queue; p != NULL; p = p->next)
+		{
+			if (Q_stricmp(p->pakname, pakname) != 0 || Q_memcmp(p->resource.rgucMD5_hash, hash, 16) != 0)
+				continue;
+
+			if (pResourceEntry)
+				Q_memcpy(pResourceEntry, &p->resource, sizeof(resource_t));
+
+			return TRUE;
+		}
+	}
+
+	Q_snprintf(name, ARRAYSIZE(name), "%s", pakname);
+
+	COM_DefaultExtension(name, HASHPAK_EXTENSION);
+	fp = FS_Open(name, "rb");
+
+	if (!fp)
+	{
+		Con_Printf("ERROR: couldn't open %s.\n", name);
+		return FALSE;
+	}
+
+	FS_Read(&header, sizeof(hash_pack_header_t), fp);
+
+	if (Q_strncmp(header.szFileStamp, "HPAK", sizeof(header.szFileStamp)))
+	{
+		Con_Printf("%s is not an HPAK file\n", name);
+		FS_Close(fp);
+		return FALSE;
+	}
+
+	if (header.version != HASHPAK_VERSION)
+	{
+		Con_Printf("HPAK_List:  version mismatch\n");
+		FS_Close(fp);
+		return FALSE;
+	}
+
+	FS_Seek(fp, header.nDirectoryOffset, FILESYSTEM_SEEK_HEAD);
+	FS_Read(&directory.nEntries, 4, fp);
+
+	if (directory.nEntries < 1 || (unsigned int)directory.nEntries > MAX_FILE_ENTRIES)
+	{
+		Con_Printf("ERROR: HPAK had bogus # of directory entries:  %i\n", directory.nEntries);
+		FS_Close(fp);
+		return FALSE;
+	}
+
+	directory.p_rgEntries = (hash_pack_entry_t*)Mem_Malloc(sizeof(hash_pack_entry_t) * directory.nEntries);
+	FS_Read(directory.p_rgEntries, sizeof(hash_pack_entry_t) * directory.nEntries, fp);
+
+	bFound = HPAK_FindResource(&directory, hash, pResourceEntry);
+
+	FS_Close(fp);
+	Mem_Free(directory.p_rgEntries);
+
+	return bFound;
 }
--- engine/hashpak.h
+++ engine/hashpak.h
@@ -3,6 +3,32 @@
 
 #include "custom.h"
 
+#define HASHPAK_EXTENSION	".hpk"
+#define HASHPAK_VERSION		0x0001
+
+#define MAX_FILE_SIZE		0x20000
+#define MAX_FILE_ENTRIES	0x8000
+
+typedef struct hash_pack_entry_s
+{
+	resource_t resource;
+	int nOffset;
+	int nFileLength;
+} hash_pack_entry_t;
+
+typedef struct hash_pack_directory_s
+{
+	int nEntries;
+	hash_pack_entry_t* p_rgEntries;
+} hash_pack_directory_t;
+
+typedef struct hash_pack_header_s
+{
+	char szFileStamp[4];
+	int version;
+	int nDirectoryOffset;
+} hash_pack_header_t;
+
 typedef struct hash_pack_queue_s
 {
 	char* pakname;
@@ -12,6 +38,8 @@ typedef struct hash_pack_queue_s
 	hash_pack_queue_s* next;
 } hash_pack_queue_t;
 
+extern hash_pack_queue_t* gp_hpak_queue;
+
 void HPAK_FlushHostQueue();
 
 void HPAK_Init();
@@ -19,7 +47,7 @@ void HPAK_Init();
 void HPAK_CheckIntegrity( const char* pakname );
 
 void HPAK_AddLump( bool bUseQueue, const char* pakname, resource_t* pResource, void* pData, FileHandle_t fpSource );
-
-int HPAK_ResourceForHash( const char* pakname, byte* hash, resource_t* pResourceEntry );
+qboolean HPAK_FindResource(hash_pack_directory_t* pDir, unsigned char* hash, struct resource_s* pResourceEntry);
+qboolean HPAK_ResourceForHash(char* pakname, unsigned char* hash, struct resource_s* pResourceEntry);
 
 #endif //ENGINE_HASHPAK_H
