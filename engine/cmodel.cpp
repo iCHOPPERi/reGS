134933e8dc63b83f8459e61f26cb3f4908cb39c9
ScriptedSnark <sevcenkobogdan855@gmail.com>
2022-03-26 19:56:25 +0500

    [engine] Implement CM_LeafPVS, CM_LeafPAS

--- engine/cmodel.cpp
+++ engine/cmodel.cpp
@@ -3,11 +3,32 @@
 
 unsigned char* gPAS;
 unsigned char* gPVS;
+int gPVSRowBytes;
 unsigned char mod_novis[MODEL_MAX_PVS];
 
 void Mod_Init()
 {
 	Q_memset(mod_novis, 255, MODEL_MAX_PVS);
+}
+
+unsigned char* CM_LeafPVS(int leafnum)
+{
+	if (gPVS)
+	{
+		return &gPVS[gPVSRowBytes * leafnum];
+	}
+
+	return mod_novis;
+}
+
+unsigned char* CM_LeafPAS(int leafnum)
+{
+	if (gPAS)
+	{
+		return &gPAS[gPVSRowBytes * leafnum];
+	}
+
+	return mod_novis;
 }
 
 void CM_FreePAS()
--- engine/cmodel.h
+++ engine/cmodel.h
@@ -6,7 +6,8 @@ extern unsigned char* gPVS;
 const int MODEL_MAX_PVS = 1024;
 
 void Mod_Init();
-
+unsigned char* CM_LeafPVS(int leafnum);
+unsigned char* CM_LeafPAS(int leafnum);
 void CM_FreePAS();
 
 #endif //ENGINE_MODEL_H
