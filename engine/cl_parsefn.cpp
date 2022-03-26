663ab24115f1cbdd4b680e6a4c42b48e3c0f174e
ScriptedSnark <sevcenkobogdan855@gmail.com>
2022-03-26 19:37:58 +0500

    [engine] Implement CL_HookEvent

--- engine/cl_parsefn.cpp
+++ engine/cl_parsefn.cpp
@@ -1,5 +1,6 @@
 #include "quakedef.h"
 #include "cl_parsefn.h"
+#include "module.h"
 
 event_hook_t* g_pEventHooks;
 
@@ -8,9 +9,53 @@ void CL_InitEventSystem()
 	g_pEventHooks = 0;
 }
 
-void CL_HookEvent( char* name, void( *pfnEvent )( event_args_t* ) )
+void CL_HookEvent(char* name, void(*pfnEvent)(event_args_t*)) // here my reverse-engineering skill is bad
 {
-	//TODO: implement - Solokiller
+    event_hook_t* evhook; // ebx
+    event_hook_t* eventhooks; // eax
+
+    g_engdstAddrs.pfnHookEvent(&name, &pfnEvent);
+
+    if (!pfnEvent)
+    {
+        Con_Printf("CL_HookEvent:  Must provide an event hook callback\n", &pfnEvent);
+        return;
+    }
+
+    if (!g_pEventHooks)
+    {
+        Con_Printf("CL_HookEvent:  Must provide a valid event name\n", &pfnEvent);
+        return;
+    }
+
+    if (name && *name)
+    {
+        while (!g_pEventHooks->name || Q_stricmp(name, g_pEventHooks->name))
+        {
+            g_pEventHooks = g_pEventHooks->next;
+            if (!g_pEventHooks)
+            {
+                evhook = (event_hook_t*)Mem_ZeroMalloc(0xCu);
+                evhook->name = Mem_Strdup(name);
+                evhook->pfnEvent = pfnEvent;
+                eventhooks = g_pEventHooks;
+                g_pEventHooks = evhook;
+                evhook->next = eventhooks;
+            }
+        }
+
+        Con_DPrintf("CL_HookEvent:  Called on existing hook, updating event hook\n");
+        g_pEventHooks->pfnEvent = pfnEvent;
+    }
+    else
+    {
+        evhook = (event_hook_t*)Mem_ZeroMalloc(sizeof(event_hook_t));
+        evhook->name = Mem_Strdup(name);
+        evhook->pfnEvent = pfnEvent;
+        eventhooks = g_pEventHooks;
+        g_pEventHooks = evhook;
+        evhook->next = eventhooks;
+    }
 }
 
 void CL_QueueEvent( int flags, int index, float delay, event_args_t* pargs )
