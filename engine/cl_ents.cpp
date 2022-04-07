1a7ccf54d2a92c8037dec1f3ce4405927ff6c56e
ScriptedSnark <sevcenkobogdan855@gmail.com>
2022-04-07 16:09:58 +0500

    [engine] Implement CL_EntityTeleported

--- engine/cl_ents.cpp
+++ engine/cl_ents.cpp
@@ -1,5 +1,6 @@
 #include "quakedef.h"
 #include "cl_ents.h"
+#include <cl_entity.h>
 
 void CL_SetSolidPlayers( int playernum )
 {
@@ -14,4 +15,14 @@ void CL_SetUpPlayerPrediction( bool dopred, bool bIncludeLocalClient )
 void CL_AddStateToEntlist( physent_t* pe, entity_state_t* state )
 {
 	//TODO: implement - Solokiller
+}
+
+qboolean CL_EntityTeleported(cl_entity_t* ent)
+{
+	if (ent->curstate.origin[0] - ent->prevstate.origin[0] <= 128.0	&& ent->curstate.origin[1] - ent->prevstate.origin[1] <= 128.0)
+	{
+		return ent->curstate.origin[2] - ent->prevstate.origin[2] > 128.0;
+	}
+
+	return true;
 }
--- engine/cl_ents.h
+++ engine/cl_ents.h
@@ -9,4 +9,6 @@ void CL_SetUpPlayerPrediction( bool dopred, bool bIncludeLocalClient );
 
 void CL_AddStateToEntlist( physent_t* pe, entity_state_t* state );
 
+qboolean CL_EntityTeleported(cl_entity_t* ent);
+
 #endif //ENGINE_CL_ENTS_H
